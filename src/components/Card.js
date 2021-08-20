c527ddcd8c6d8ffc2d1618331d891d714f9e9a50
Nolo Marsh <nolo.marsh@gmail.com>
2021-08-20 14:53:51 -0400

    Added Card component

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+// import axios from 'axios'
+// import './css/Card.css'
+
+const Card = props => {
+    let [card, setCard] = useState({...props.card})
+
+    return(
+        <div className='listCard'>
+            <p>{card.name}</p>
+        </div>
+    )
+}
+
+export default Card
