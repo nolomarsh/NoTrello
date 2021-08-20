e603c3c65cae53360ffd27a2ebfab366da98fafb
Kristian <kristianreyes7@icloud.com>
2021-08-20 09:57:59 -0700

    style changes to list

--- src/App.js
+++ src/App.js
@@ -6,47 +6,52 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 
 const App = () => {
-    let [cards, setCards] = useState([])
+    let [list, setList] = useState([])
 
-    const handleCreate = (addCards) => {
+    const handleCreate = (addList) => {
         axios
-        .post('notrello-backend.herokuapp.com/api/list', addCards)
-        .the((response) => {
+        .post('http://notrello-backend.herokuapp.com/api/list', addList)
+        .then((response) => {
             console.log(response)
-            getCards()
+            getList()
         })
     }
 
-    const getCards = () => {
-        axios.get('notrello-backend.herokuapp.com/api/list')
-        .then(
-        (response) => setCards(response.data),
-        (error) => console.error(error)
-        )
+    const getList = () => {
+        axios.get('http://notrello-backend.herokuapp.com/api/list')
+        .then((response) => setList(response.data),
+            (error) => console.error(error))
         .catch((error) => console.error(error))
     }
 
     useEffect(() => {
-        getCards()
+        getList()
     }, [])
 
 return (
-    <>
+    <div className="container wrapper">
+    
         <Header />
-        <Add handleCreate={handleCreate} />
-        <div className="cards">
-            {cards.map((cards) => {
-                return (
-                    <div className="cards"
-                        key={cards.id}>
-                        <h4>Title: {cards.title}</h4>
-                    </div>
 
+        <Add handleCreate={handleCreate} />
+
+        <div className="main container-fluid">
+            {list.map((listItem) => {
+                return (
+
+                    <div className="lists"
+                        key={listItem.id}>
+                        <h3 className="list_title">Title: {listItem.title}</h3>
+                        <p className="list_description">Description: {listItem.description}</p>
+
+                    </div>
                 )
             })}
         </div>
+
         <Footer />
-    </>
+    
+    </div>
 )
 
 }
