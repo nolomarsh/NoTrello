73428f6391a032d51e82b8f884db3532d861aa49
Nolo Marsh <nolo.marsh@gmail.com>
2021-08-22 14:10:08 -0400

    Added delete button to list-edit

--- src/components/Edit_list.js
+++ src/components/Edit_list.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import {useRefreshData} from '../DataContext'
 
 const Edit_list = (props) => {
-    let emptylist = props.list 
+    let emptylist = props.list
     let [list, setList] = useState(emptylist)
 
     const refreshData = useRefreshData()
@@ -30,7 +30,14 @@ const Edit_list = (props) => {
 
     const exitEdit = () => {
         props.setListView('cards')
+    }
 
+    const handleDelete = () => {
+        axios
+            .delete('https://notrello-backend.herokuapp.com/api/list/' + list.id)
+            .then((response) => {
+                refreshData()
+            })
     }
 
     return (
@@ -62,6 +69,7 @@ const Edit_list = (props) => {
 
                 <input className='btn btn-warning' type="submit" />
                 <button className='btn btn-warning' onClick={exitEdit}>Back</button>
+                <button className='btn btn-warning' onClick={handleDelete}>Delete</button>
             </form>
 
         </>
