e85f5ad871f393da0c32cd177da08f24fbbd759a
Nolo Marsh <nolo.marsh@gmail.com>
2021-08-22 15:53:18 -0400

    Added button to cards to toggle card details

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,10 +4,24 @@ import './css/Card.css'
 
 const Card = props => {
     let [card] = useState({...props.card})
+    let [cardView, setCardView] = useState('mini')
+
+    const toggleCardDetails = () => {
+        cardView === 'mini' ? setCardView('details') : setCardView('mini')
+    }
 
     return(
         <div className='listCard'>
-            <p>{card.name}</p>
+            <div className='headLine'>
+                <p>{card.name}</p>
+                <p onClick={toggleCardDetails}>...</p>
+            </div>
+            {cardView === 'details' &&
+            <>
+                <p>{card.body}</p>
+                <p>{card.labels}</p>
+            </>
+            }
         </div>
     )
 }
